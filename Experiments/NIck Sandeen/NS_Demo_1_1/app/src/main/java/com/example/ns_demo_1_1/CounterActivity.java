0e7d7f0dd3c912ab48d38528bb6e951892086eb5
Nick Sandeen <nsandeen@iastate.edu>
2021-09-03 17:12:38 -0500

    Initial commit of NS_Demo_1_1

---
+++ Experiments/NIck Sandeen/NS_Demo_1_1/app/src/main/java/com/example/ns_demo_1_1/CounterActivity.java
@@ -0,0 +1,18 @@
+package com.example.ns_demo_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+
+
+
+
+    }
+}
---
+++ Experiments/NIck Sandeen/NS_Demo_1_1/app/src/main/java/com/example/ns_demo_1_1/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.ns_demo_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.mainActivity_buttonCounter);
+        b2 = (Button) findViewById(R.id.mainActivity_buttonLogin);
+
+        b1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, CounterActivity.class);
+                startActivity(i);
+            }
+        });
+
+        b2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello this is not actually the counter", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    };
+}

6410c528808ac79cb7f65ec775633a70eec135af
Nick Sandeen <nsandeen@iastate.edu>
2021-09-26 12:56:24 -0500

    Added navigation and added Google Play Services for Google Maps integration.

--- Frontend/app/src/main/java/com/example/cyhelp/MainActivity.java
+++ Frontend/app/src/main/java/com/example/cyhelp/MainActivity.java
@@ -5,22 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.userButton);
+        b2 = (Button) findViewById(R.id.techButton);
+
+        b1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent userIntent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(userIntent);
+            }
+        });
+
+        b2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent techIntent = new Intent(MainActivity.this, TechTicketFinderMapActivity.class);
+                startActivity(techIntent);
+            }
+        });
+
+
     }
 
-    public void techActivity(View view) {
-        Intent techIntent = new Intent(this, TechActivity.class);
-        startActivity(techIntent);
-    }
 
-    public void userActivity(View view) {
-        Intent userIntent = new Intent(this, UserActivity.class);
-        startActivity(userIntent);
-    }
 }

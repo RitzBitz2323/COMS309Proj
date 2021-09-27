28f3fde7c27ec1766b9f97abfd4e2ee03d117dbf
BuildTools <unconfigured@null.spigotmc.org>
2021-09-26 23:22:44 -0500

    just actor classes now

--- Backend/Test_RoundTrip/test/src/main/java/coms309/roundTrip/repository/TriviaRepository.java
+++
@@ -1,9 +0,0 @@
-package coms309.roundTrip.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import coms309.roundTrip.model.Trivia;
-
-public interface TriviaRepository extends JpaRepository<Trivia, Long>{
-
-}

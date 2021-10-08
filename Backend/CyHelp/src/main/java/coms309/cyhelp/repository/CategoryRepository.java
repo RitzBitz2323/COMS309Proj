842170d0e93c278f900b88a65cf1d1d5695d6fb6
Ritvik Ambekar <rambekar@iastate.edu>
2021-10-07 21:12:01 -0500

    Created branch and created CategoryRepository and initalized Category Model and CategoryController

---
+++ Backend/CyHelp/src/main/java/coms309/cyhelp/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package coms309.cyhelp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import coms309.cyhelp.model.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+	
+	Category findById(int id);
+	
+	
+}

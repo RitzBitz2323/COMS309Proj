782cc35992a9fc40b9aa24fea623efd7035721dc
Parthiv Ganguly <parthiv@iastate.edu>
2021-10-08 14:26:16 -0500

    Integrated OSMDroid into App

--- Frontend/app/src/main/java/com/example/cyhelp/MainActivity.java
+++ Frontend/app/src/main/java/com/example/cyhelp/MainActivity.java
@@ -39,5 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void loadTechMap(View view) {
+        Intent intent = new Intent(this, TechActivity.class);
+        startActivity(intent);
+    }
+
 
 }

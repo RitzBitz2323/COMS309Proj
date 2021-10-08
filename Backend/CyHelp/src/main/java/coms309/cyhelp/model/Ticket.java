81ece57796a3f51250d655c933cb57b935aca0a4
Ritvik Ambekar <rambekar@iastate.edu>
2021-10-08 12:31:23 -0500

    Commented a onetoone relation on Ticket.java and will confirm on it later this Oct 8th

--- Backend/CyHelp/src/main/java/coms309/cyhelp/model/Ticket.java
+++ Backend/CyHelp/src/main/java/coms309/cyhelp/model/Ticket.java
@@ -7,6 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -45,6 +46,7 @@ public class Ticket {
 	private Actor technicianActor;
 	
 	@Column(name = "category")
+	//@OneToOne
 	private String category;
 	
 	

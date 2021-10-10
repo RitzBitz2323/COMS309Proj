3df8f629838dabe52c719eff3dd1012342b53cfe
Ritvik Ambekar <rambekar@iastate.edu>
2021-10-10 17:21:05 -0500

    Completed the creation of the Category Controller and inputed default values into Category Table

--- Backend/CyHelp/src/main/java/coms309/cyhelp/controller/CategoryController.java
+++ Backend/CyHelp/src/main/java/coms309/cyhelp/controller/CategoryController.java
@@ -1,5 +1,45 @@
 package coms309.cyhelp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import coms309.cyhelp.model.Category;
+
+import coms309.cyhelp.repository.CategoryRepository;
+
+@RestController
 public class CategoryController {
 
+	@Autowired
+	CategoryRepository categoryRepository;
+
+	
+
+	/**
+	 * Gets all of the category in the repository.
+	 * @return
+	 */
+	@GetMapping("/categories")
+	public List<Category> getAllCategories() {
+		return categoryRepository.findAll();
+	}
+	
+	/**
+	 * Returns the category with the specified id. 
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/categories/{id}")
+	public Category getCategory(@PathVariable int id) {
+		return categoryRepository.findById(id);
+	}
+	
 }

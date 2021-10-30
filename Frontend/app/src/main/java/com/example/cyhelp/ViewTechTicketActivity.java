c6571e67f11df7a4719b530808d4c3f58f860fd4
Parthiv Ganguly <parthiv@iastate.edu>
2021-10-29 22:51:35 -0500

    Now Tech can View Ticket Details

--- Frontend/app/src/main/java/com/example/cyhelp/ViewTechTicketActivity.java
+++ Frontend/app/src/main/java/com/example/cyhelp/ViewTechTicketActivity.java
@@ -4,8 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class ViewTechTicketActivity extends AppCompatActivity {
+
+    String url = "http://coms-309-051.cs.iastate.edu:8080/tickets/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +32,39 @@ public class ViewTechTicketActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         int id = intent.getIntExtra("id", 2);
+        url += id;
         System.out.println("ID of ticket: " + id);
+
+        ArrayList<String> jsonResponses = new ArrayList<> ();
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    System.out.println(response);
+                    String title = response.getString("title");
+                    String description = response.getString("description");
+                    String address = response.getJSONObject("customer").getString("homeAddress");
+
+                    TextView titleView = (TextView) findViewById(R.id.title);
+                    TextView descriptionView = (TextView) findViewById(R.id.description);
+                    TextView addressView = (TextView) findViewById(R.id.address);
+
+                    titleView.setText(title);
+                    descriptionView.setText(description);
+                    addressView.setText(address);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(jsonObjectRequest);
     }
 }

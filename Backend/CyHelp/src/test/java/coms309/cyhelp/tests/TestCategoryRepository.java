4d63370c54429df65763cdd48983688be519e42a
Ritvik Ambekar <rambekar@iastate.edu>
2021-10-31 17:34:57 -0500

    maybe fixed error

--- Backend/CyHelp/src/test/java/coms309/cyhelp/tests/TestCategoryRepository.java
+++ Backend/CyHelp/src/test/java/coms309/cyhelp/tests/TestCategoryRepository.java
@@ -22,24 +22,29 @@ public class TestCategoryRepository {
 		
 		Ticket ticket1 = mock(Ticket.class);
 		when(ticket1.getTitle()).thenReturn("Laptop Screen Broken");
+		//when(categoryRepository.findById(0).getTicket(0)).thenReturn(ticket1);
 		when(ticket1.getDescription()).thenReturn("Played football with my mac and accidentally broke it");
+		//when(categoryRepository.findById(0).getTicket(0)).thenReturn(ticket1);
 		when(ticket1.getCustomer()).thenReturn(null);
+		//when(categoryRepository.findById(0).getTicket(0)).thenReturn(ticket1);
 		when(ticket1.getLatitude()).thenReturn(19.0);
+		//when(categoryRepository.findById(0).getTicket(0)).thenReturn(ticket1);
 		when(ticket1.getLongitude()).thenReturn(42.0);
+		//when(categoryRepository.findById(0).getTicket(0)).thenReturn(ticket1);
 		
 		Ticket ticket2 = mock(Ticket.class);
-		when(ticket1.getTitle()).thenReturn("Sink pipes burst");
-		when(ticket1.getDescription()).thenReturn("Tried cleaning the hood of my car in the sink");
-		when(ticket1.getCustomer()).thenReturn(null);
-		when(ticket1.getLatitude()).thenReturn(21.0);
-		when(ticket1.getLongitude()).thenReturn(29.0);
+		when(ticket2.getTitle()).thenReturn("Sink pipes burst");
+		when(ticket2.getDescription()).thenReturn("Tried cleaning the hood of my car in the sink");
+		when(ticket2.getCustomer()).thenReturn(null);
+		when(ticket2.getLatitude()).thenReturn(21.0);
+		when(ticket2.getLongitude()).thenReturn(29.0);
 		
 		Ticket ticket3 = mock(Ticket.class);
-		when(ticket1.getTitle()).thenReturn("Patio on fire");
-		when(ticket1.getDescription()).thenReturn("Accidentally lit my patio on fire");
-		when(ticket1.getCustomer()).thenReturn(null);
-		when(ticket1.getLatitude()).thenReturn(44.0);
-		when(ticket1.getLongitude()).thenReturn(-20.0);
+		when(ticket3.getTitle()).thenReturn("Patio on fire");
+		when(ticket3.getDescription()).thenReturn("Accidentally lit my patio on fire");
+		when(ticket3.getCustomer()).thenReturn(null);
+		when(ticket3.getLatitude()).thenReturn(44.0);
+		when(ticket3.getLongitude()).thenReturn(-20.0);
 		
 		Category categoryTest = categoryRepository.findById(0);
 		categoryTest.addTicket(ticket1);

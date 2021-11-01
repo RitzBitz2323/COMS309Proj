56eab42460904560e55de9b5b0e92acc4233f4cd
Ritvik Ambekar <rambekar@iastate.edu>
2021-11-01 12:14:30 -0500

    cleaned up imports

--- Backend/CyHelp/src/test/java/coms309/cyhelp/tests/TestCategoryRepository.java
+++ Backend/CyHelp/src/test/java/coms309/cyhelp/tests/TestCategoryRepository.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 import coms309.cyhelp.model.Category;

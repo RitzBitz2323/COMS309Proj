85e4e07169ae54d67fa3b88c6dda463881855bc3
Nick Sandeen <nsandeen@iastate.edu>
2021-11-02 10:29:32 -0500

    Added additional Mock tests

--- Frontend/app/src/test/java/com/example/cyhelp/SignUpModelTest.java
+++ Frontend/app/src/test/java/com/example/cyhelp/SignUpModelTest.java
@@ -23,7 +23,7 @@ public class SignUpModelTest extends TestCase {
 
 
     @Test
-    public void signupResponseTest_returnsTrue() throws JSONException {
+    public void signupResponseTest_returnsEmpty() throws JSONException {
 
         SignUpPresenter test = mock(SignUpPresenter.class);
         SignUpActivity testActivity = mock(SignUpActivity.class);
@@ -36,7 +36,7 @@ public class SignUpModelTest extends TestCase {
         String firstNameCorrect = "Nick";
         String lastNameCorrect = "Sandeen";
         String passwordCorrect = "Password";
-        String addressCorrect = "Password";
+        String addressCorrect = "Ames";
         int ActorType = 0;
 
         JSONObject testObject = new JSONObject();
@@ -57,10 +57,110 @@ public class SignUpModelTest extends TestCase {
         assertEquals("", ErrorMessage);
     }
 
+    @Test
+    public void signupResponseTest_returnsErrorUserAlreadyTaken() throws JSONException {
 
+        SignUpPresenter test = mock(SignUpPresenter.class);
+        SignUpActivity testActivity = mock(SignUpActivity.class);
 
+        SignUpModel model = new SignUpModel(testActivity.getApplicationContext());
 
+        String usernameCorrect = "nsandeen";
+        String firstNameCorrect = "Nick";
+        String lastNameCorrect = "Sandeen";
+        String passwordCorrect = "Password";
+        String addressCorrect = "Ames";
+        int ActorType = 0;
 
+        JSONObject testObject = new JSONObject();
+        testObject.put("username", usernameCorrect);
+        testObject.put("firstName", firstNameCorrect);
+        testObject.put("lastName", lastNameCorrect);
+        testObject.put("password", passwordCorrect);
+        testObject.put("homeAddress", addressCorrect);
+        testObject.put("userType", ActorType);
+
+        doNothing().when(test).SignUpUser(usernameCorrect, firstNameCorrect, lastNameCorrect, passwordCorrect, addressCorrect, ActorType);
+
+        model.httpCreateUserRequest(testObject, test);
+
+        SignUpModel model2 = new SignUpModel(testActivity.getApplicationContext());
+        model2.httpCreateUserRequest(testObject, test);
+
+        verify(test, times(1)).getActorInfo();
+
+        String ErrorMessage = model2.getErrorMessage();
+        //Username is expected to be taken. The error is actually a JSON Parse Error so it could be caused by other things if the presenter does not behave as expected.
+        assertEquals("Username already exists! Please enter a new username.", ErrorMessage);
+    }
+
+    @Test
+    public void signupResponseTest_returnsErrorInvalidUserType() throws JSONException {
+
+        SignUpPresenter test = mock(SignUpPresenter.class);
+        SignUpActivity testActivity = mock(SignUpActivity.class);
+
+        SignUpModel model = new SignUpModel(testActivity.getApplicationContext());
+
+        String usernameCorrect = "nsandeen";
+        String firstNameCorrect = "Nick";
+        String lastNameCorrect = "Sandeen";
+        String passwordCorrect = "Password";
+        String addressCorrect = "Ames";
+        int ActorType = 5;
+
+        JSONObject testObject = new JSONObject();
+        testObject.put("username", usernameCorrect);
+        testObject.put("firstName", firstNameCorrect);
+        testObject.put("lastName", lastNameCorrect);
+        testObject.put("password", passwordCorrect);
+        testObject.put("homeAddress", addressCorrect);
+        testObject.put("userType", ActorType);
+
+        doNothing().when(test).SignUpUser(usernameCorrect, firstNameCorrect, lastNameCorrect, passwordCorrect, addressCorrect, ActorType);
+
+        model.httpCreateUserRequest(testObject, test);
+
+        verify(test, times(1)).getActorInfo();
+
+        String ErrorMessage = model.getErrorMessage();
+        //Parse Error Expected as userType of 5 does not exist.
+        assertEquals("Username already exists! Please enter a new username.", ErrorMessage);
+    }
+
+    @Test
+    public void signupResponseTest_returnsValidUserInfo() throws JSONException {
+
+        SignUpPresenter test = mock(SignUpPresenter.class);
+        SignUpActivity testActivity = mock(SignUpActivity.class);
+
+        SignUpModel model = new SignUpModel(testActivity.getApplicationContext());
+
+        String usernameCorrect = "nsandeen";
+        String firstNameCorrect = "Nick";
+        String lastNameCorrect = "Sandeen";
+        String passwordCorrect = "Password";
+        String addressCorrect = "Ames";
+        int ActorType = 5;
+
+        JSONObject testObject = new JSONObject();
+        testObject.put("username", usernameCorrect);
+        testObject.put("firstName", firstNameCorrect);
+        testObject.put("lastName", lastNameCorrect);
+        testObject.put("password", passwordCorrect);
+        testObject.put("homeAddress", addressCorrect);
+        testObject.put("userType", ActorType);
+
+        doNothing().when(test).SignUpUser(usernameCorrect, firstNameCorrect, lastNameCorrect, passwordCorrect, addressCorrect, ActorType);
+
+        model.httpCreateUserRequest(testObject, test);
+
+        verify(test, times(1)).getActorInfo();
+
+        String ErrorMessage = model.getErrorMessage();
+        //Parse Error Expected as userType of 5 does not exist.
+        assertEquals("Username already exists! Please enter a new username.", ErrorMessage);
+    }
 
 
 
